ef639733a633aeebb1c5476fbe96a35b0b3c109d
kelsaur <kellyb.karner@gmail.com>
2025-09-17 12:04:00 +0200

    feat: final getAllBookings & getBookingById with validation and error handling

--- functions/getAllBookings/index.mjs
+++ functions/getAllBookings/index.mjs
@@ -3,6 +3,13 @@ import { QueryCommand } from "@aws-sdk/lib-dynamodb";
 
 export const handler = async (event) => {
 	try {
+		const formatDate = (dateStr) =>
+			new Date(dateStr).toLocaleDateString("en-GB");
+		const headers = {
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "*",
+		};
+
 		const command = new QueryCommand({
 			TableName: "HotelBooking",
 			KeyConditionExpression: "PK = :PK",
@@ -12,27 +19,33 @@ export const handler = async (event) => {
 		});
 
 		const result = await docClient.send(command);
-		const totalBookings = result.Count;
+		const bookingsData = result.Items || []; //in case of no bookings returns empty array instead of undefined
+		const totalBookings = result.Count ?? bookingsData.length;
 
-		const bookings = result.Items.map((item) => ({
-			bookingId: item.bookingId,
-			guestName: item.guestName,
-			checkIn: item.checkIn,
-			checkOut: item.checkOut,
-			guestCount: parseInt(item.guestCount),
-			roomType: item.roomType,
-			totalPrice: parseInt(item.totalPrice),
-		}));
-
-		if (bookings.length === 0) {
+		if (bookingsData.length === 0) {
 			return {
 				statusCode: 200,
+				headers,
 				body: JSON.stringify({ message: "There are currently no bookings." }),
 			};
 		}
 
+		const bookings = bookingsData.map((item) => ({
+			"Booking ID": item.bookingId,
+			"Guest Name": item.guestName,
+			"Guest Email": item.guestEmail,
+			"Number Of Rooms": item.numberOfRooms,
+			"Number Of Guests": item.guestCount,
+			"Room Type": item.roomType,
+			"Check In": formatDate(item.checkIn),
+			"Check Out": formatDate(item.checkOut),
+			"Booking Status": item.status,
+			"Created At": formatDate(item.createdAt),
+		}));
+
 		return {
 			statusCode: 200,
+			headers,
 			body: JSON.stringify({
 				message: `There are currently ${totalBookings} bookings in total: `,
 				bookings,
@@ -41,6 +54,7 @@ export const handler = async (event) => {
 	} catch (error) {
 		return {
 			statusCode: 500,
+			headers,
 			body: JSON.stringify({ error: "Failed to fetch bookings." }),
 		};
 	}
--- functions/getBookingById/index.mjs
+++ functions/getBookingById/index.mjs
@@ -6,10 +6,28 @@ export const handler = async (event) => {
 		const bookingId = event.pathParameters.bookingId;
 		console.log("Received booking ID: ", bookingId);
 
+		const isUuid =
+			/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+		const formatDate = (dateStr) =>
+			new Date(dateStr).toLocaleDateString("en-GB");
+		const headers = {
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "*",
+		};
+
 		if (!bookingId) {
 			return {
 				statusCode: 400,
+				headers,
 				body: JSON.stringify({ message: "Missing booking ID." }),
+			};
+		}
+
+		if (!isUuid.test(bookingId)) {
+			return {
+				statusCode: 400,
+				headers,
+				body: JSON.stringify({ message: "Invalid booking ID format." }),
 			};
 		}
 
@@ -27,19 +45,33 @@ export const handler = async (event) => {
 		if (!result.Item) {
 			return {
 				statusCode: 404,
+				headers,
 				body: JSON.stringify({ message: "There is no such booking." }),
 			};
 		}
 
 		return {
+			headers,
 			statusCode: 200,
 			body: JSON.stringify({
-				booking: result.Item,
+				booking: {
+					"Booking ID": result.Item.bookingId,
+					"Guest Name": result.Item.guestName,
+					"Guest Email": result.Item.guestEmail,
+					"Number Of Rooms": result.Item.numberOfRooms,
+					"Number Of Guests": result.Item.guestCount,
+					"Room Type": result.Item.roomType,
+					"Check In": formatDate(result.Item.checkIn),
+					"Check Out": formatDate(result.Item.checkOut),
+					"Booking Status": result.Item.status,
+					"Created At": formatDate(result.Item.createdAt),
+				},
 			}),
 		};
 	} catch (error) {
 		return {
 			statusCode: 500,
+			headers,
 			body: JSON.stringify({ error: "Failed to fetch this booking." }),
 		};
 	}
